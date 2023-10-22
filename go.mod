0b48c7d935912762a33d8eeb63af0cd304d44c2b
chtiwa <chtiwaa@gmail.com>
2023-10-22 14:38:12 +0100

    chi router and middleware

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/chtiwa/go_microservices
 
 go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/go-chi/chi/v5 v5.0.10
+)
--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", basicHandler)
 	// saving it as a pointer and taking the memory address
 	server := &http.Server{
 		Addr:    ":8000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
