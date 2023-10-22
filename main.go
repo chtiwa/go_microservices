bda4cf1957c0d342138bd78efff9f215881c15b8
chtiwa <chtiwaa@gmail.com>
2023-10-22 14:26:22 +0100

    added basicHandler

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello")
+	// saving it as a pointer and taking the memory address
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Failed to listen to server", err)
+	}
+
+}
+
+// r is a pointer of http.Request
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello world!"))
 }
